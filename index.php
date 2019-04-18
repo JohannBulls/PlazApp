4d404aac05fc07347ea2596cf3bbc035c30c722c
JOHANN AMAYA LOPEZ <jaamaya643@misena.edu.co>
2019-04-17 22:38:17 -0500

    Master

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!--
+* @version v1.0
+* Plazapp
+* Copyright (c) 2018
+* Licensed under MIT (https://github.com/JohannBulls)
+-->
+<html lang="es">
+<?php
+include 'view/head.php'; ?>
+<title>PLazapp</title>
+
+<body class="app header-fixed sidebar-fixed aside-menu-fixed sidebar-lg-show">
+  <?php
+  include 'view/header.php'; ?>
+  <div class="app-body">
+    <?php
+    include 'view/sidebar.php'; ?>
+    <main class="main">
+      <div class="container-fluid">
+
+
+
+      </div>
+    </main>
+  </div>
+  <?php
+  include 'view/footer.php'; ?>
