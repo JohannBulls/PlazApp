fd68bec4548b6c46c123189453e9dc2333022fd3
JOHANN AMAYA LOPEZ <jaamaya643@misena.edu.co>
2019-04-17 22:40:14 -0500

    Css
    linea de espacio comentario

--- view/head.php
+++ view/head.php
@@ -11,6 +11,6 @@
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
   <!--Boostrap-->
   <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
-  <!-- Css Coreui-->
+  <!--Css Coreui-->
   <link href="./assets/css/style.css" rel="stylesheet" />
   <link href="./vendors/pace-progress/css/pace.min.css" rel="stylesheet" />
