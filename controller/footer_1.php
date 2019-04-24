314030e6f3794b2eaaa32ed23351097028b0eb85
JOHANN AMAYA LOPEZ <jaamaya643@misena.edu.co>
2019-04-24 16:23:31 -0500

    Controller

---
+++ controller/footer_1.php
@@ -0,0 +1,18 @@
+<footer class="app-footer">
+  <div>
+    <a href="https://github.com/JohannBulls">
+    <span>&copy;</a> 2019 PlazApp</span>
+  </div>
+</footer>
+<!-- Bootstrap js-->
+<script src="./../../assets/js/jquery-3.4.0.min.js"></script>
+<script src="./../../assets/js/bootstrap.min.js"></script>
+<!-- CoreUI js-->
+<script src="./../../node_modules/popper.js/dist/umd/popper.min.js"></script>
+<script src="./../../node_modules/pace-progress/pace.min.js"></script>
+<script src="./../../node_modules/perfect-scrollbar/dist/perfect-scrollbar.min.js"></script>
+<script src="./../../node_modules/@coreui/coreui/dist/js/coreui.min.js"></script>
+
+</body>
+
+</html>
---
+++ controller/sidebar_1.php
@@ -0,0 +1,29 @@
+<div class="sidebar">
+      <nav class="sidebar-nav">
+        <ul class="nav">
+          <li class="nav-item">
+            <a class="nav-link" href="index.html">
+              <i class="nav-icon far fa-user-circle"></i> Perfil
+            </a>
+            <li class="nav-title"></li>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="./index.php">
+              <i class="nav-icon fas fa-home"></i> Inicio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="typography.html">
+              <i class="nav-icon fas fa-book"></i> Reseta</a>
+          </li>
+          <li class="nav-item nav-dropdown">
+            <a class="nav-link " href="#">
+              <i class="nav-icon fas fa-bicycle"></i> Domiciliario</a>
+          </li>
+          <li class="nav-item nav-dropdown">
+            <a class="nav-link " href="#">
+              <i class="nav-icon fas fa-info-circle"></i> Informacion</a>
+          </li>
+        </ul>
+      </nav>
+      <button class="sidebar-minimizer brand-minimizer" type="button"></button>
+    </div>
