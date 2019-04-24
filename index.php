2d8160d9761c396fd942a950701161046666692d
JOHANN AMAYA LOPEZ <jaamaya643@misena.edu.co>
2019-04-23 22:20:19 -0500

    Index Pagina de Inicio

--- index.php
+++ index.php
@@ -6,23 +6,195 @@
 * Licensed under MIT (https://github.com/JohannBulls)
 -->
 <html lang="es">
-<?php
+  <?php
 include 'view/head.php'; ?>
-<title>PLazapp</title>
+  <title>PLazapp</title>
 
-<body class="app header-fixed sidebar-fixed aside-menu-fixed sidebar-lg-show">
-  <?php
-  include 'view/header.php'; ?>
-  <div class="app-body">
+  <body class="app header-fixed sidebar-fixed aside-menu-fixed sidebar-lg-show">
     <?php
+  include 'view/header.php'; ?>
+    <div class="app-body">
+      <?php
     include 'view/sidebar.php'; ?>
-    <main class="main">
-      <div class="container-fluid">
-
-Hola mundo
-
-      </div>
-    </main>
-  </div>
-  <?php
+      <main class="main">
+        <div class="col-12 d-flex justify-content-center text-center p-5">
+          <img
+            src="assets/img/brand/logo.png"
+            alt="logo plazapp"
+            width="50%"
+            height="40%"
+          />
+        </div>
+        <div class="d-flex justify-content-center text-center p-2">
+          <div class="col-6 d-flex">
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Buscar Producto"
+              aria-label="Recipient's username"
+              aria-describedby="button-addon2"
+            />
+            <div class="input-group-append">
+              <button
+                class="btn btn-outline-secondary"
+                type="button"
+                id="button-addon2"
+              >
+                <i class="fas fa-search"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="d-flex justify-content-center pt-4">
+          <h1 class="text-danger">¡Ofertas!</h1>
+        </div>
+        <div class="container-fluid justify-content-center">
+          <div class="row">
+            <div class="col-sm-6 col-md p-3">
+              <img
+                src="assets/img/productos/1.jpg"
+                alt="Cebolla Larga"
+                width="90%"
+                class="rounded-lg"
+              />
+            </div>
+            <div class="col-sm-6 col-md p-3">
+              <img
+                src="assets/img/productos/2.jpg"
+                alt="Cebolla Larga"
+                width="90%"
+                class="rounded-lg"
+              />
+            </div>
+            <div class="col-sm-6 col-md p-3">
+              <img
+                src="assets/img/productos/3.png"
+                alt="Cebolla Larga"
+                width="90%"
+                class="rounded-lg"
+              />
+            </div>
+            <div class="col-sm-6 col-md p-3">
+              <img
+                src="assets/img/productos/4.jpg"
+                alt="Cebolla Larga"
+                width="90%"
+                class="rounded-lg"
+              />
+            </div>
+          </div>
+        </div>
+        <div class="container-fluid justify-content-center">
+          <div class="row">
+            <div class="col-sm-6 col-md p-3">
+              <img
+                src="assets/img/productos/5.jpg"
+                alt="Cebolla Larga"
+                width="90%"
+                class="rounded-lg"
+              />
+            </div>
+            <div class="col-sm-6 col-md p-3">
+              <img
+                src="assets/img/productos/6.jpg"
+                alt="Cebolla Larga"
+                width="90%"
+                class="rounded-lg"
+              />
+            </div>
+            <div class="col-sm-6 col-md p-3">
+              <img
+                src="assets/img/productos/7.jpg"
+                alt="Cebolla Larga"
+                width="90%"
+                class="rounded-lg"
+              />
+            </div>
+            <div class="col-sm-6 col-md p-3">
+              <img
+                src="assets/img/productos/8.jpg"
+                alt="Cebolla Larga"
+                width="90%"
+                class="rounded-lg"
+              />
+            </div>
+          </div>
+        </div>
+        <div class="container-fluid justify-content-center">
+          <div class="row">
+            <div class="col-sm-6 col-md p-3">
+              <img
+                src="assets/img/productos/9.jpg"
+                alt="Cebolla Larga"
+                width="90%"
+                class="rounded-lg"
+              />
+            </div>
+            <div class="col-sm-6 col-md p-3">
+              <img
+                src="assets/img/productos/10.jpg"
+                alt="Cebolla Larga"
+                width="90%"
+                class="rounded-lg"
+              />
+            </div>
+            <div class="col-sm-6 col-md p-3">
+              <img
+                src="assets/img/productos/11.jpg"
+                alt="Cebolla Larga"
+                width="90%"
+                class="rounded-lg"
+              />
+            </div>
+            <div class="col-sm-6 col-md p-3">
+              <img
+                src="assets/img/productos/12.jpg"
+                alt="Cebolla Larga"
+                width="90%"
+                class="rounded-lg"
+              />
+            </div>
+          </div>
+        </div>
+        <div class="container-fluid justify-content-center">
+          <div class="row">
+            <div class="col-sm-6 col-md p-3">
+              <img
+                src="assets/img/productos/13.jpg"
+                alt="Cebolla Larga"
+                width="90%"
+                class="rounded-lg"
+              />
+            </div>
+            <div class="col-sm-6 col-md p-3">
+              <img
+                src="assets/img/productos/14.jpg"
+                alt="Cebolla Larga"
+                width="90%"
+                class="rounded-lg"
+              />
+            </div>
+            <div class="col-sm-6 col-md p-3">
+              <img
+                src="assets/img/productos/15.jpg"
+                alt="Cebolla Larga"
+                width="90%"
+                class="rounded-lg"
+              />
+            </div>
+            <div class="col-sm-6 col-md p-3">
+              <img
+                src="assets/img/productos/16.jpg"
+                alt="Cebolla Larga"
+                width="90%"
+                class="rounded-lg"
+              />
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+    <?php
   include 'view/footer.php'; ?>
+  </body>
+</html>
